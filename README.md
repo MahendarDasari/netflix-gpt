eff593fe4d47d72bab4081fcd8d981c6252872dc
mahendardasari040@gmail.com <mahendardasari040@gmail.com>
2024-03-03 10:36:40 +0530

    test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 -Browse
     -Header
     -Main movie
+ ---PWD: Playground@1989
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <h1 className="text-2xl font-bold underline bg-blue  text-green-700">
-    Hello Mahnedar!
-  </h1>
+      Hello Mahnedar!
+    </h1>
   );
 }
 
